3caa1d5c2ca342d07a509b82e1edfe04692ea74e
renhubgit <renik2001@gmail.com>
2022-09-13 17:32:28 +0200

    added logic for creating todo lists

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hi")
+import { v4 as uuidv4 } from "uuid"
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  date: Date
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    date: new Date(),
+  }
+
+  addListItem(newTask)
+})
+
+function addListItem(newTask: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  label.append(checkbox, newTask.title)
+  item.append(label)
+  list?.append(item)
+}
